c290a44fe7261c8b78e5020b4de7d207039f5dd4
Julio Martins de Sousa <juliomartinssousa@gmail.com>
2018-01-07 12:02:11 -0200

    Finalização da configuração Redux nas telas de autenticação

--- src/reducers/AutenticacaoReducer.js
+++ src/reducers/AutenticacaoReducer.js
@@ -11,6 +11,9 @@ export default (state = INITIAL_STATE, action) => {
 	if (action.type == 'modifica_senha') {
 		return { ...state, senha: action.payload }
 	}
+	if (action.type == 'modifica_nome') {
+		return { ...state, nome: action.payload }
+	}
 	
 	return state;
 }
