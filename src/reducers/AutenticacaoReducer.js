88fee26eddb2455fecee13490819081640094338
Julio Martins de Sousa <juliomartinssousa@gmail.com>
2018-01-06 23:42:06 -0200

    Instalação, configuração e primeiros passos com o Redux

---
+++ src/reducers/AutenticacaoReducer.js
@@ -0,0 +1,16 @@
+const INITIAL_STATE = {
+	nome: 'Julio',
+	email: 'juliomsousa@blackcoffee.com',
+	senha: 'abc123'
+}
+
+export default (state = INITIAL_STATE, action) => {
+	if (action.type == 'modifica_email') {
+		return { ...state, email: action.payload }
+	}
+	if (action.type == 'modifica_senha') {
+		return { ...state, senha: action.payload }
+	}
+	
+	return state;
+}
