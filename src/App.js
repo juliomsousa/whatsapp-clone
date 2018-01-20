a45b66ba7700cf0ca1ad5ac484256dab5165020a
Julio Martins de Sousa <juliomartinssousa@gmail.com>
2018-01-20 10:24:58 -0200

    editando FormCadastro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';
 
-export default props => (
-	<Provider store={createStore(reducers)} >
-		<Routes/>
-	</Provider>
-)
+class App extends Component {
+	
+	componentWillMount() {
+		// Initialize Firebase
+		var config = {
+			apiKey: "AIzaSyABQAVuyEcHkBCeD7jSy-I-X7MZ8aESZ5U",
+			authDomain: "whatsapp-lone.firebaseapp.com",
+			databaseURL: "https://whatsapp-lone.firebaseio.com",
+			projectId: "whatsapp-lone",
+			storageBucket: "whatsapp-lone.appspot.com",
+			messagingSenderId: "202407415156"
+		};
+		firebase.initializeApp(config);
+	}
+
+	render() {
+		return(
+			<Provider store={createStore(reducers)} >
+				<Routes/>
+			</Provider>
+		);
+	}	
+}
+
+export default App;
--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,41 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, TextInput, Button, Image } from 'react-native';
 import { connect } from 'react-redux';
-import { modificaEmail, modificaSenha, modificaNome } from '../actions/AutenticacaoActions';
+import {
+	modificaEmail,
+	modificaSenha,
+	modificaNome,
+	cadastraUsuario
+} from '../actions/AutenticacaoActions';
 
-const formCadastro = props => (
-	<Image style={{ flex: 1, width: null }} source={require('../img/bg.png')}>
-		<View style={{ flex: 1, padding: 10 }}>
-			<View style={{ flex: 4, justifyContent: 'center' }}>
-				<TextInput
-					value={props.nome}
-					placeholder="Nome"
-					placeholderTextColor='#FFF'
-					style={{ fontSize: 20, height: 45 }}
-					onChangeText={texto => props.modificaNome(texto)}
-				/>
-				<TextInput
-					value={props.email}
-					placeholder="E-mail"
-					placeholderTextColor='#FFF'
-					style={{ fontSize: 20, height: 45 }}
-					onChangeText={texto => props.modificaEmail(texto)}	
-				/>
-				<TextInput
-					secureTextEntry
-					value={props.senha}
-					placeholder="Senha"
-					placeholderTextColor='#FFF'
-					style={{ fontSize: 20, height: 45 }}
-					onChangeText={texto => props.modificaSenha(texto)}
-				/>
-			</View>
-			<View style={{ flex: 1 }}>
-				<Button title="Cadastrar" color="#115E54" onPress={() => false} />
-			</View>
-		</View>
-	</Image>
-);
+class formCadastro extends Component {
+
+	_cadastraUsuario() {
+
+		const { nome, email, senha } = this.props;
+		
+		this.props.cadastraUsuario({ nome, email, senha });
+	}
+
+	render() {
+		return(
+			<Image style={{ flex: 1, width: null }} source={require('../img/bg.png')}>
+				<View style={{ flex: 1, padding: 10 }}>
+					<View style={{ flex: 4, justifyContent: 'center' }}>
+						<TextInput
+							value={this.props.nome}
+							placeholder="Nome"
+							placeholderTextColor='#FFF'
+							style={{ fontSize: 20, height: 45 }}
+							onChangeText={texto => this.props.modificaNome(texto)}
+						/>
+						<TextInput
+							value={this.props.email}
+							placeholder="E-mail"
+							placeholderTextColor='#FFF'
+							style={{ fontSize: 20, height: 45 }}
+							onChangeText={texto => this.props.modificaEmail(texto)}	
+						/>
+						<TextInput
+							secureTextEntry
+							value={this.props.senha}
+							placeholder="Senha"
+							placeholderTextColor='#FFF'
+							style={{ fontSize: 20, height: 45 }}
+							onChangeText={texto => this.props.modificaSenha(texto)}
+						/>
+					</View>
+					<View style={{ flex: 1 }}>
+						<Button
+							title="Cadastrar"
+							color="#115E54"
+							onPress={() => this._cadastraUsuario()}
+						/>
+					</View>
+				</View>
+			</Image>
+		);
+	}
+}
 
 const mapStateToProps = state => (
 	{
@@ -45,4 +66,12 @@ const mapStateToProps = state => (
 	}
 )
 
-export default connect(mapStateToProps, { modificaEmail, modificaSenha, modificaNome})(formCadastro);
+export default connect(
+	mapStateToProps,
+	{
+		modificaEmail,
+		modificaSenha,
+		modificaNome,
+		cadastraUsuario
+	}
+)(formCadastro);
